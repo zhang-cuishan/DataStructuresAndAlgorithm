ea9c0d3279bec4fcac8c10628f617f1b163a0fa9
SpringChang <290794272@qq.com>
2020-12-23 00:56:35 +0800

    Initial commit

---
+++ src/com/springchang/datastructures/Main.java
@@ -0,0 +1,13 @@
+package com.springchang.datastructures;
+
+/**
+ * @author : 张翠山
+ * @Descption:
+ * @date 2020/12/23
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
