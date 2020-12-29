da87da5a246ae32f05d15541dc754d036999bae0
SpringChang <290794272@qq.com>
2020-12-29 14:47:03 +0800

    1.选择排序算法的实现 2.测试用例 3.数组工具类编写

---
+++ src/com/springchang/datastructures/sort/ArrayUtils.java
@@ -0,0 +1,16 @@
+package com.springchang.datastructures.sort;
+
+/**
+ * 数组工具类
+ * @author : 张翠山
+ */
+public class ArrayUtils {
+
+    //控制台输出数组
+    public static void print(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
---
+++ src/com/springchang/datastructures/sort/SelectionSort.java
@@ -0,0 +1,45 @@
+package com.springchang.datastructures.sort;
+
+import java.util.Arrays;
+
+/**
+ * 选择排序
+ * @author : 张翠山
+ */
+public class SelectionSort {
+
+    public static void selectionSort(int[] arr) {
+
+        //index标记正在标记的位置
+        for (int index = 0; index < arr.length; index++) {
+
+            //一趟查询，查找最小值的下标index和最小值value
+            int minIndex = index;
+            int minValue = arr[index];
+            for (int i = index+1; i < arr.length; i++) {
+                if(arr[i] < minValue) {
+                    minIndex = i;
+                    minValue = arr[i];
+                }
+            }
+
+            //交换一下最小值和当前index值
+            if(index != minIndex) {
+                arr[minIndex] = arr[index];
+                arr[index] = minValue;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 1, 4, 5, 7, 2, 3};
+
+        System.out.println("排序前数组：");
+        ArrayUtils.print(arr);
+
+        selectionSort(arr);
+
+        System.out.println("排序后数组:");
+        ArrayUtils.print(arr);
+    }
+}
