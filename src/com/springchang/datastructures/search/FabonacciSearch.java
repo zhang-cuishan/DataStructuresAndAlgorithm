9fbcb5055b3eecae5d84262f7141d79ae56698a9
SpringChang <290794272@qq.com>
2021-02-05 03:18:28 +0800

    斐波那契搜索算法实现，已经测试BUG.

--- src/com/springchang/datastructures/search/FabonacciSearch.java
+++ src/com/springchang/datastructures/search/FabonacciSearch.java
@@ -11,7 +11,7 @@ public class FabonacciSearch {
     static final int MAX_SIZE = 20;
 
     //获取斐波那契数列前20个数据
-    public static int[] fabonacci() {
+    private static int[] fabonacci() {
         int[] arr = new int[MAX_SIZE];
 
         arr[0] = arr[1] = 1;
@@ -22,49 +22,55 @@ public class FabonacciSearch {
         return arr;
     }
 
+    /**
+     * 斐波那契搜索算法
+     * @param arr
+     * @param key
+     * @return
+     */
+    public static int fabonacciSearch(int[] arr, int key) {
+
+        int low = 0;
+        int high = arr.length-1;
+
+        //1.获取斐波那契数列
+        int[] fabArr = fabonacci();
+
+        //2.计算k的位置
+        int k = 0;
+        while(fabArr[k] < high) {
+            k++;
+        }
+
+        //创建新数组，并补齐fabArr[k]个元素
+        int[] temp = Arrays.copyOf(arr, fabArr[k]);
+        for (int i = high+1; i < temp.length; i++) {
+            temp[i] = arr[high];
+        }
+
+        while(low <= high) {
+            int mid = low + fabArr[k-1]-1;
+            if(temp[mid] > key) {
+                high = mid-1;
+                k--;
+            } else if(temp[mid] < key) {
+                low = mid+1;
+                k -= 2;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
 
         int[] arr = new int[] {10, 12, 13, 14, 15, 17, 18};
 
-        int key = 17;
+        int key = 16;
 
-        //斐波那契数列
-        int[] fabonacciArray = fabonacci();
-
-        int low = 0;
-
-        //最后一个元素的下标
-        int high = arr.length-1;
-
-        int k = 0;
-
-        while(fabonacciArray[k]-1 <= high) {
-            k++;
-        }
-
-        //用一个新的数组来保持数据
-        int lenth = fabonacciArray[k+1]-1;
-        int[] temp = Arrays.copyOf(arr,lenth);
-        for (int i = high; i < temp.length; i++) {
-            temp[i] = temp[high];
-        }
-
-        //计算黄金分割点的下标
-        int mid = low + fabonacciArray[k-1]-1;
-
-        while(low < mid) {
-            if(temp[mid] > key) {
-                high = mid-1;
-                k -= 1;
-            } else if(temp[mid] < key) {
-                low = low+1;
-                k -= 2;
-            } else {
-                System.out.println("找到下标:"+mid);
-                return;
-            }
-            mid = low + fabonacciArray[k-1]-1;
-        }
-        System.out.println("没有找到数据"+key);
+        int index = fabonacciSearch(arr, key);
+        System.out.printf("元素 %d 的小标值为 %d\n", key, index);
     }
 }
