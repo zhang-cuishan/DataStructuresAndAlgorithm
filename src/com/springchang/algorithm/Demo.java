325e81d002d38cfe956bc16e5e9dd0127d4524e4
SpringChang <290794272@qq.com>
2021-02-05 02:39:35 +0800

    test.

---
+++ src/com/springchang/algorithm/Demo.java
@@ -0,0 +1,11 @@
+package com.springchang.algorithm;
+
+/**
+ * @author : 张翠山
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
